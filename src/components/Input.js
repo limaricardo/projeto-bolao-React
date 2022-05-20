909e8f2a926e97d2e85e80a7a913f9781d8fc663
Ricardo <limaricardop@gmail.com>
2022-05-19 23:10:44 -0300

    Input component

---
+++ src/components/Input.js
@@ -0,0 +1,23 @@
+import { SelectionState } from "draft-js";
+import React, { useState } from "react";
+
+const Input = ({ htmlFor, type, id, name, children }) => {
+  const [term, setTerm] = useState("");
+
+  return (
+    <div>
+      <div className="container-input">
+        <label htmlFor={htmlFor}>{children}</label>
+        <input
+          onChange={(e) => setTerm(e.target.value)}
+          type={type}
+          id={id}
+          name={name}
+          value={term}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Input;
